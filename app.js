f4360bab667a44e871aeb1ba2ff3a31d9c0c4336
Phillip Gibson <phillip.gibson@hey.com>
2021-08-25 14:00:06 -0400

    try again

--- app.js
+++ app.js
@@ -4,21 +4,38 @@ const fs = require('fs').promises
 const run = async () => {
   const browser = await puppeteer.launch({ headless: true })
   const [page] = await browser.pages()
-  await page.goto('https://www.bcsdschools.net/Page/34941', { waitUntil: 'networkidle2' })
+  await page.goto('https://www.bcsdschools.net/Page/34941', {
+    waitUntil: 'networkidle2'
+  })
   await page.select('#Value1_1', 'Westview Primary')
   await page.click("input[name='searchID']")
   await page.waitForSelector('tr.cbResultSetDataRow')
 
-  const [school,,,staff,student,updated] = await page.$$eval('tr.cbResultSetDataRow td', tds => tds.map(i => i.innerText))
-  
+  const [
+    school,
+    ,
+    ,
+    staff,
+    student,
+    updated
+  ] = await page.$$eval('tr.cbResultSetDataRow td', tds =>
+    tds.map(i => i.innerText)
+  )
+
   console.log('school:', school)
   console.log('staff:', staff)
   console.log('student:', student)
   console.log('updated:', updated)
 
-  await fs.writeFile('./out.json', JSON.stringify({
-    school, staff, student, updated
-  }))
+  await fs.writeFile(
+    './stats.json',
+    JSON.stringify({
+      school,
+      staff,
+      student,
+      updated
+    })
+  )
 
   await browser.close()
 }
