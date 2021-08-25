8e9038e9d7f0d976f497881a0b09443109d156ad
Phillip Gibson <phillip.gibson@hey.com>
2021-08-25 13:37:41 -0400

    try schedule

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const fetch = require('node-fetch')
+const puppeteer = require('puppeteer')
+const fs = require('fs').promises
 
 const run = async () => {
-  const res = await fetch('https://www.bcsdschools.net/Page/34941')
-  const text = await res.text()
+  const browser = await puppeteer.launch({ headless: true })
+  const [page] = await browser.pages()
+  await page.goto('https://www.bcsdschools.net/Page/34941', { waitUntil: 'networkidle2' })
+  await page.select('#Value1_1', 'Westview Primary')
+  await page.click("input[name='searchID']")
+  await page.waitForSelector('tr.cbResultSetDataRow')
 
-  console.log('text:', text)
+  const [school,,,staff,student,updated] = await page.$$eval('tr.cbResultSetDataRow td', tds => tds.map(i => i.innerText))
+  
+  console.log('school:', school)
+  console.log('staff:', staff)
+  console.log('student:', student)
+  console.log('updated:', updated)
+
+  await fs.writeFile('./out.json', JSON.stringify({
+    school, staff, student, updated
+  }))
+
+  await browser.close()
 }
 
 run()
